b9b9c2100966d8b6aa84ab16e6a76af8e2ef5cc6
Emmy <emmy@Emmys-MacBook-Air.local>
2018-06-25 19:25:31 +0200

    creating project skeleton and Adding firebase SDK

---
+++ app/src/main/java/tests/com/journal/app/activities/JNMainActivity.java
@@ -0,0 +1,13 @@
+package com.journal.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JNMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.jn_main_activity);
+    }
+}
